3c1404c65d02bf305a3460a90ece8b27742a9b06
shannonc22 <g00358845@gmit.ie>
2020-01-20 17:28:34 +0000

    added for loop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -3,8 +3,11 @@ package ie.gmit.packagename;
 class HelloWorld {
 
     public static void main(String[] args) {
-        // Print Hello World! to the console.
+        // Print Shannon! to the console.
+        for(int i=1; i<=10; i++){
         System.out.println("Hello World!");
+        
+        }
     }
 
 }
