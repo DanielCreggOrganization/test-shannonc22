bb5dab49cd4724e096bd6d0a3e2db8e04644c509
shannonc22 <g00358845@gmit.ie>
2020-01-23 14:40:47 +0000

    add new method

---
+++ src/ie/gmit/packagename/MyClass.java
@@ -0,0 +1,16 @@
+package ie.gmit.packagename;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+             myMethod();
+        }
+       
+    }
+
+    public static void myMethod(){
+        System.out.println("I just got executed!");
+    }
+
+}
